726bd81c95ce568e221f3ed01235607be5a3ade8
lukinatored <lukepoirrier1@gmail.com>
2026-04-24 17:05:07 +0200

    Fix Categorie entity - clean version

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -1,11 +1,14 @@
-<?php
 
+<?php
 namespace App\Entity;
 
 use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
+#[ORM\Table(name: 'categorie')]
 class Categorie
 {
     #[ORM\Id]
@@ -13,18 +16,31 @@ class Categorie
     #[ORM\Column]
     private ?int $id = null;
 
-<<<<<<< HEAD
-=======
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 100, unique: true)]
     private ?string $nom = null;
 
->>>>>>> 43fbb94 (Initial project Symfony boutique)
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class)]
+    #[ORM\JoinColumn(name: 'parent_id', referencedColumnName: 'id', nullable: true)]
+    private ?self $parent = null;
+
+    #[ORM\OneToMany(mappedBy: 'categorie', targetEntity: Montre::class)]
+    private Collection $montres;
+
+    public function __construct()
+    {
+        $this->montres = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
     }
-<<<<<<< HEAD
-=======
 
     public function getNom(): ?string
     {
@@ -34,8 +50,63 @@ class Categorie
     public function setNom(string $nom): static
     {
         $this->nom = $nom;
-
         return $this;
     }
->>>>>>> 43fbb94 (Initial project Symfony boutique)
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): static
+    {
+        $this->parent = $parent;
+        return $this;
+    }
+
+    public function getMontres(): Collection
+    {
+        return $this->montres;
+    }
+
+    public function addMontre(Montre $montre): static
+    {
+        if (!$this->montres->contains($montre)) {
+            $this->montres->add($montre);
+            $montre->setCategorie($this);
+        }
+        return $this;
+    }
+
+    public function removeMontre(Montre $montre): static
+    {
+        if ($this->montres->removeElement($montre)) {
+            if ($montre->getCategorie() === $this) {
+                $montre->setCategorie(null);
+            }
+        }
+        return $this;
+    }
 }
